4935cb7c1370489954999b4c6a63004f8686baac
hoya9802 <hoya9802@gmail.com>
2023-12-04 21:17:58 +0900

    update code

--- network.py
+++ network.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 class bn_relu_conv(nn.Module):
     def __init__(self, c_in, c_out, kernel=1, st=1, pad=0):
         super(bn_relu_conv, self).__init__()
+
         self.bn_relu_conv = nn.Sequential(
             nn.BatchNorm2d(c_in),
             nn.ReLU(),
@@ -19,10 +20,8 @@ class Transition(nn.Module):
     def __init__(self, c_in, c_out):
         super(Transition, self).__init__()
 
-        self.c_in = c_in
-        self.c_out = c_out
         self.bn = nn.BatchNorm2d(c_in)
-        self.conv = nn.Conv2d(self.c_in, self.c_out, kernel_size=(1,1))
+        self.conv = nn.Conv2d(c_in, c_out//2, kernel_size=(1,1))
         self.avg = nn.AvgPool2d(kernel_size=(2, 2), stride=2)
 
     def forward(self, x):
@@ -32,48 +31,59 @@ class Transition(nn.Module):
         
         return x
     
-class Bottleneck(nn.Module):
-    def __init__(self, c_in, c_out):
-        super(Bottleneck, self).__init__()
-
-        self.c_in = c_in
-        self.c_out = c_out
-
-        self.bn_relu_conv1 = bn_relu_conv(self.c_in, self.c_out * 4,)
-        self.bn_relu_conv2 = bn_relu_conv(self.c_out * 4, self.c_out, kernel=3, pad=1)
+class DenseBlock(nn.Module):
+    def __init__(self, c_in, k):
+        super(DenseBlock, self).__init__()
+        
+        self.bn_relu_conv1 = bn_relu_conv(c_in, k * 4)   
+        self.bn_relu_conv2 = bn_relu_conv(k * 4, k, kernel=3, pad=1)
 
     def forward(self, x):
         out = self.bn_relu_conv1(x)
         out = self.bn_relu_conv2(out)
-        out = torch.cat((x, out), 1)
+        out = torch.cat((x, out), 1) 
         return out
 
 
 class DenseNet121(nn.Module):
-    def __init__(self, num_class):
+    def __init__(self, num_class, k):
         super(DenseNet121, self).__init__()
 
-        self.bn = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU()
-        self.conv = nn.Conv2d(3, 64, kernel_size=(7, 7), stride=(2, 2), padding=3, bias=False)
-        self.maxp = nn.MaxPool2d(kernel_size=(3, 3), stride=2, padding=1)
+        self.num_class = num_class
+        self.k = k
+        self.conv = nn.Conv2d(3, k*2, kernel_size=(7, 7), stride=(2, 2), padding=3, bias=False)  # [B, 3]
+        self.maxp = nn.MaxPool2d(kernel_size=(3, 3), stride=(2, 2), padding=1)                   # [B, 64]
 
-        self.fc = nn.Linear(, num_class)
+        self.densenet = nn.Sequential(
+                DenseBlock(64, self.k),
+                DenseBlock(96, self.k),
+                DenseBlock(128, self.k),
+                DenseBlock(160, self.k),
+                DenseBlock(192, self.k),
+                DenseBlock(224, self.k),
+
+                Transition(256, 128),
+
+                DenseBlock(128, self.k),
+                DenseBlock(160, self.k),
+                DenseBlock(192, self.k),
+                DenseBlock(224, self.k),
+                DenseBlock(256, self.k),
+                DenseBlock(288, self.k),
+                DenseBlock(320, self.k),
+                DenseBlock(352, self.k),
+                DenseBlock(384, self.k),
+                DenseBlock(416, self.k),
+                DenseBlock(448, self.k),
+                DenseBlock(480, self.k)
+        )
+
+        self.fc = nn.Linear(512, num_class)
 
     def forward(self, x):
         x = self.conv(x)
-        x = self.bn(x)
-        x = self.relu(x)
         x = self.maxp(x)
-
-        x = DenseBlock(64, , 6)
-        x = Transition(, )
-        x = DenseBlock(, , 12)
-        x = Transition(, )
-        x = DenseBlock(, , 24)
-        x = Transition(, )
-        x = DenseBlock(, , 16)
-
+        x = self.densenet(x)
         x = torch.mean(x, dim=(2, 3))
         x = self.fc(x)
 
